29afb4e0b04d6876065bb9c32ee19b94fa2f08b2
Creatza <125740024+Creatza@users.noreply.github.com>
2023-06-15 21:39:44 +0300

    update

--- My_project.py
+++ My_project.py
@@ -1,90 +1,105 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-
 class Login(unittest.TestCase):
     def setUp(self):
-        self.driver = By.Chrome()  # Poți înlocui cu driverul preferat (Firefox, etc.)
+        self.driver = webdriver.Chrome()
         self.driver.get("https://the-internet.herokuapp.com/")
         self.driver.find_element(By.LINK_TEXT, "Form Authentication").click()
 
     def tearDown(self):
         self.driver.quit()
 
-    def test_1_verify_url(self):
+    def test_url(self):
         expected_url = "https://the-internet.herokuapp.com/login"
-        self.assertEqual(self.driver.current_url, expected_url)
+        actual_url = self.driver.current_url
+        self.assertEqual(actual_url, expected_url, "URL is incorrect")
 
-    def test_2_verify_page_title(self):
+    def test_page_title(self):
         expected_title = "The Internet"
-        self.assertEqual(self.driver.title, expected_title)
+        actual_title = self.driver.title
+        self.assertEqual(actual_title, expected_title, "Page title is incorrect")
 
-    def test_3_verify_heading_text(self):
+    def test_heading_text(self):
         expected_text = "Login Page"
-        heading = self.driver.find_element(By.XPATH, "//h2")
-        self.assertEqual(heading.text, expected_text)
+        actual_text = self.driver.find_element(By.XPATH, "//h2").text
+        self.assertEqual(actual_text, expected_text, "Heading text is incorrect")
 
-    def test_4_verify_login_button_displayed(self):
-        login_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-        self.assertTrue(login_button.is_displayed())
+    def test_login_button_displayed(self):
+        login_button = self.driver.find_element(By.XPATH, "//button[@type='submit']")
+        self.assertTrue(login_button.is_displayed(), "Login button is not displayed")
 
-    def test_5_verify_link_href_attribute(self):
+    def test_elemental_selenium_link_href(self):
         expected_href = "http://elementalselenium.com/"
         link = self.driver.find_element(By.LINK_TEXT, "Elemental Selenium")
-        self.assertEqual(link.get_attribute("href"), expected_href)
+        actual_href = link.get_attribute("href")
+        self.assertEqual(actual_href, expected_href, "Elemental Selenium link href is incorrect")
 
-    def test_6_verify_error_displayed_empty_credentials(self):
-        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-        error_message = self.driver.find_element(By.ID, "flash")
-        self.assertTrue(error_message.is_displayed())
-
-    def test_7_verify_error_message_with_invalid_credentials(self):
-        expected_error_message = "Your username is invalid!"
-        self.driver.find_element(By.ID, "username").send_keys("invalid")
-        self.driver.find_element(By.ID, "password").send_keys("invalid")
-        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    def test_empty_credentials_error(self):
+        self.driver.find_element(By.CSS_SELECTOR, "#username").clear()
+        self.driver.find_element(By.CSS_SELECTOR, "#password").clear()
+        self.driver.find_element(By.CSS_SELECTOR, "#login > button").click()
         error_message = self.driver.find_element(By.ID, "flash").text
-        self.assertTrue(expected_error_message in error_message,
-                        "Error message text is incorrect")
+        self.assertTrue("Your username is invalid!" in error_message, "Error message text is incorrect")
 
-    def test_8_verify_error_disappears_after_close(self):
-        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".close").click()
-        error_message = self.driver.find_elements(By.ID, "flash")
-        self.assertEqual(len(error_message), 0)
+    def test_invalid_credentials_error(self):
+        username_input = self.driver.find_element(By.CSS_SELECTOR, "#username")
+        password_input = self.driver.find_element(By.CSS_SELECTOR, "#password")
+        username_input.clear()
+        password_input.clear()
+        username_input.send_keys("invalid_user")
+        password_input.send_keys("invalid_password")
+        self.driver.find_element(By.CSS_SELECTOR, "#login > button").click()
+        error_message = self.driver.find_element(By.ID, "flash").text
+        expected_error_message = "Your username is invalid!"
+        self.assertTrue(expected_error_message in error_message, "Error message text is incorrect")
 
-    def test_9_verify_labels_text(self):
+    def test_dismiss_error_message(self):
+        self.driver.find_element(By.CSS_SELECTOR, "#username").clear()
+        self.driver.find_element(By.CSS_SELECTOR, "#password").clear()
+        self.driver.find_element(By.CSS_SELECTOR, "#login > button").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".flash > .close").click()
+        error_message = self.driver.find_element(By.ID, "flash").text
+        self.assertEqual(error_message, "", "Error message is still displayed after dismissing")
+
+    def test_label_texts(self):
         expected_labels = ["Username", "Password"]
         labels = self.driver.find_elements(By.XPATH, "//label")
         actual_labels = [label.text for label in labels]
-        self.assertListEqual(actual_labels, expected_labels)
+        self.assertListEqual(actual_labels, expected_labels, "Label texts are incorrect")
 
-    def test_10_verify_successful_login(self):
+    def test_successful_login(self):
+        username_input = self.driver.find_element(By.CSS_SELECTOR, "#username")
+        password_input = self.driver.find_element(By.CSS_SELECTOR, "#password")
+        username_input.clear()
+        password_input.clear()
+        username_input.send_keys("valid_user")
+        password_input.send_keys("valid_password")
+        self.driver.find_element(By.CSS_SELECTOR, "#login > button").click()
         expected_url = "https://the-internet.herokuapp.com/secure"
-        self.driver.find_element(By.ID, "username").send_keys("validuser")
-        self.driver.find_element(By.ID, "password").send_keys("validpass")
-        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-        self.assertIn(expected_url, self.driver.current_url)
+        actual_url = self.driver.current_url
+        self.assertIn(expected_url, actual_url, "URL does not contain '/secure'")
+        success_message = WebDriverWait(self.driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "flash.success"))).text
+        self.assertTrue(success_message.startswith("You logged into a secure area!"),
+                        "Success message is incorrect")
 
-        wait = unittest(self.driver, 10)
-        success_message = wait.until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "flash.success"))
-        )
-        self.assertTrue(success_message.is_displayed())
-        self.assertIn("secure area!", success_message.text)
-
-    def test_11_verify_logout(self):
-        self.driver.find_element(By.ID, "username").send_keys("validuser")
-        self.driver.find_element(By.ID, "password").send_keys("validpass")
-        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+    def test_logout(self):
+        username_input = self.driver.find_element(By.CSS_SELECTOR, "#username")
+        password_input = self.driver.find_element(By.CSS_SELECTOR, "#password")
+        username_input.clear()
+        password_input.clear()
+        username_input.send_keys("valid_user")
+        password_input.send_keys("valid_password")
+        self.driver.find_element(By.CSS_SELECTOR, "#login > button").click()
+        self.driver.find_element(By.XPATH, "//a[contains(text(), 'Logout')]").click()
         expected_url = "https://the-internet.herokuapp.com/login"
-        self.assertEqual(self.driver.current_url, expected_url)
-
+        actual_url = self.driver.current_url
+        self.assertEqual(actual_url, expected_url, "Did not navigate to the login page")
 
 if __name__ == "__main__":
     unittest.main()
